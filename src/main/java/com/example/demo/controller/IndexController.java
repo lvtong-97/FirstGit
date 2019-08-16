8ba356a1dea2c69f27e1a67afebf3716541f841d
lvtong <1728016616@qq.com>
2019-08-16 16:00:20 +0800

    Merge branch 'master' of https://github.com/lvtong-97/gitdemo
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -15,7 +15,7 @@ public class IndexController {
     @RequestMapping("/index")
     public String index(Model model){
         model.addAttribute("name","nihao");
-        return  "show1";
+        return "show1";
     }
 
 }
